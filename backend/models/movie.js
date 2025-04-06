7d6b5fa752d642100e32a1be37dcc93de406b63c
Руслан Ускембаев <ruslanuskembaev@MacBook-Pro-Ruslan.local>
2025-04-06 23:48:17 +0700

    nu

--- backend/models/movie.js
+++ backend/models/movie.js
@@ -1,24 +1,9 @@
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, 'Title is required'],
-  },
-  genre: {
-    type: String,
-    required: [true, 'Genre is required'],
-  },
-  releaseDate: {
-    type: Date,
-    required: [true, 'Release date is required'],
-  },
-  description: {
-    type: String,
-    required: false,
-  },
-}, { timestamps: true });
+  title: { type: String, required: true },
+  genre: { type: String, required: true },
+});
 
 const Movie = mongoose.model('Movie', movieSchema);
-
 module.exports = Movie;
